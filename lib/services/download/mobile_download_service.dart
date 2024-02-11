6d8e5bda0b6d561850d90de84cb5e96c9bbaa198
HANAI tohru <tohru@reedom.com>
2024-02-11 22:33:05 +0900

    fix

--- lib/services/download/mobile_download_service.dart
+++ lib/services/download/mobile_download_service.dart
@@ -23,14 +23,15 @@ import 'package:seasoning/services/podcast/podcast_service.dart';
 /// An implementation of a [DownloadService] that handles downloading
 /// of episodes on mobile.
 class MobileDownloadService extends DownloadService {
-
-  MobileDownloadService(
-      {required this.repository,
-      required this.downloadManager,
-      required this.podcastService,}) {
+  MobileDownloadService({
+    required this.repository,
+    required this.downloadManager,
+    required this.podcastService,
+  }) {
     downloadManager.downloadProgress.pipe(downloadProgress);
     downloadProgress.listen(_updateDownloadProgress);
   }
+
   static BehaviorSubject<DownloadProgress> downloadProgress =
       BehaviorSubject<DownloadProgress>();
 
@@ -47,28 +48,28 @@ class MobileDownloadService extends DownloadService {
   @override
   Future<bool> downloadEpisode(Episode episode) async {
     try {
-      final season = episode.season > 0 ? episode.season.toString() : '';
-      final epno = episode.episode > 0 ? episode.episode.toString() : '';
-      var dirty = false;
+      final season = episode.season != null ? episode.season.toString() : '';
+      final epno = episode.episode != null ? episode.episode.toString() : '';
 
+      var newEpisode = episode;
       if (await hasStoragePermission()) {
         // If this episode contains chapter, fetch them first.
         if (episode.hasChapters && episode.chaptersUrl != null) {
           final chapters =
               await podcastService.loadChaptersByUrl(url: episode.chaptersUrl!);
 
-          episode.chapters = chapters;
-
-          dirty = true;
+          newEpisode = newEpisode.copyWith(chapters: chapters);
         }
 
         // Next, if the episode supports transcripts download that next
         if (episode.hasTranscripts) {
           var sub = episode.transcriptUrls.firstWhereOrNull(
-              (element) => element.type == TranscriptFormat.json,);
+            (element) => element.type == TranscriptFormat.json,
+          );
 
           sub ??= episode.transcriptUrls.firstWhereOrNull(
-              (element) => element.type == TranscriptFormat.subrip,);
+            (element) => element.type == TranscriptFormat.subrip,
+          );
 
           if (sub != null) {
             var transcript =
@@ -76,34 +77,38 @@ class MobileDownloadService extends DownloadService {
 
             transcript = await podcastService.saveTranscript(transcript);
 
-            episode.transcript = transcript;
-            episode.transcriptId = transcript.id;
-
-            dirty = true;
+            newEpisode = newEpisode.copyWith(
+              transcript: transcript,
+              transcriptId: transcript.id,
+            );
           }
         }
 
-        if (dirty) {
-          await podcastService.saveEpisode(episode);
+        if (episode != newEpisode) {
+          await podcastService.saveEpisode(newEpisode);
         }
 
-        final episodePath = await resolveDirectory(episode: episode);
+        final episodePath = await resolveDirectory(episode: newEpisode);
         final downloadPath =
-            await resolveDirectory(episode: episode, full: true);
-        var uri = Uri.parse(episode.contentUrl!);
+            await resolveDirectory(episode: newEpisode, full: true);
+        var uri = Uri.parse(newEpisode.contentUrl!);
 
         // Ensure the download directory exists
-        await createDownloadDirectory(episode);
+        await createDownloadDirectory(newEpisode);
 
         // Filename should be last segment of URI.
-        var filename = safeFile(uri.pathSegments
-            .lastWhereOrNull((e) => e.toLowerCase().endsWith('.mp3')),);
+        var filename = safeFile(
+          uri.pathSegments
+              .lastWhereOrNull((e) => e.toLowerCase().endsWith('.mp3')),
+        );
 
-        filename ??= safeFile(uri.pathSegments
-            .lastWhereOrNull((e) => e.toLowerCase().endsWith('.m4a')),);
+        filename ??= safeFile(
+          uri.pathSegments
+              .lastWhereOrNull((e) => e.toLowerCase().endsWith('.m4a')),
+        );
 
         if (filename == null) {
-          //TODO: Handle unsupported format.
+          // TODO(unknown): Handle unsupported format.
         } else {
           // The last segment could also be a full URL. Take a second pass.
           if (filename.contains('/')) {
@@ -117,7 +122,8 @@ class MobileDownloadService extends DownloadService {
 
           // Some podcasts use the same file name for each episode. If we have a
           // season and/or episode number provided by iTunes we can use that. We
-          // will also append the filename with the publication date if available.
+          // will also append the filename with the publication date if
+          // available.
           var pubDate = '';
 
           if (episode.publicationDate != null) {
@@ -128,29 +134,34 @@ class MobileDownloadService extends DownloadService {
           filename = '$season$epno$pubDate$filename';
 
           log.fine(
-              'Download episode (${episode.title}) $filename to $downloadPath/$filename',);
+            'Download episode (${episode.title}) $filename to $downloadPath/$filename',
+          );
 
-          /// If we get a redirect to an http endpoint the download will fail. Let's fully resolve
-          /// the URL before calling download and ensure it is https.
+          /// If we get a redirect to an http endpoint the download will fail.
+          /// Let's fully resolve the URL before calling download and ensure it
+          /// is https.
           final url = await resolveUrl(episode.contentUrl!, forceHttps: true);
 
           final taskId =
               await downloadManager.enqueueTask(url, downloadPath, filename);
 
           // Update the episode with download data
-          episode.filepath = episodePath;
-          episode.filename = filename;
-          episode.downloadTaskId = taskId;
-          episode.downloadState = DownloadState.downloading;
-          episode.downloadPercentage = 0;
+          newEpisode = newEpisode.copyWith(
+            filepath: episodePath,
+            filename: filename,
+            downloadTaskId: taskId,
+            downloadState: DownloadState.downloading,
+            downloadPercentage: 0,
+          );
 
-          await repository.saveEpisode(episode);
+          await repository.saveEpisode(newEpisode);
 
           return true;
         }
       }
 
       return false;
+      // ignore: avoid_catches_without_on_clauses
     } catch (e, stack) {
       log.warning('Episode download failed (${episode.title})', e, stack);
       return false;
@@ -165,29 +176,36 @@ class MobileDownloadService extends DownloadService {
   Future<void> _updateDownloadProgress(DownloadProgress progress) async {
     final episode = await repository.findEpisodeByTaskId(progress.id);
 
-    if (episode != null) {
-      // We might be called during the cleanup routine during startup.
-      // Do not bother updating if nothing has changed.
-      if (episode.downloadPercentage != progress.percentage ||
-          episode.downloadState != progress.status) {
-        episode.downloadPercentage = progress.percentage;
-        episode.downloadState = progress.status;
+    if (episode == null) {
+      return;
+    }
 
-        if (progress.percentage == 100) {
-          if (await hasStoragePermission()) {
-            final filename = await resolvePath(episode);
+    // We might be called during the cleanup routine during startup.
+    // Do not bother updating if nothing has changed.
+    if (episode.downloadPercentage == progress.percentage &&
+        episode.downloadState == progress.status) {
+      return;
+    }
 
-            // If we do not have a duration for this file - let's calculate it
-            if (episode.duration == 0) {
-              final mp3Info = MP3Processor.fromFile(File(filename));
+    var newEpisode = episode.copyWith(
+      downloadPercentage: progress.percentage,
+      downloadState: progress.status,
+    );
 
-              episode.duration = mp3Info.duration.inSeconds;
-            }
-          }
+    if (progress.percentage == 100) {
+      if (await hasStoragePermission()) {
+        final filename = await resolvePath(episode);
+
+        // If we do not have a duration for this file - let's calculate it
+        if (episode.duration == 0) {
+          final mp3Info = MP3Processor.fromFile(File(filename));
+
+          newEpisode =
+              newEpisode.copyWith(duration: mp3Info.duration.inSeconds);
         }
-
-        await repository.saveEpisode(episode);
       }
     }
+
+    await repository.saveEpisode(newEpisode);
   }
 }
