81c557a4ad8728f00a2a7cdfa3a96ef2326b2535
HANAI tohru <tohru@reedom.com>
2024-02-13 17:27:24 +0900

    chore: gen

--- lib/entities/episode.freezed.dart
+++ lib/entities/episode.freezed.dart
@@ -12,7 +12,7 @@ part of 'episode.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 Episode _$EpisodeFromJson(Map<String, dynamic> json) {
   return _Episode.fromJson(json);
--- lib/entities/sleep.freezed.dart
+++ lib/entities/sleep.freezed.dart
@@ -12,7 +12,7 @@ part of 'sleep.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 Sleep _$SleepFromJson(Map<String, dynamic> json) {
   return _Sleep.fromJson(json);
