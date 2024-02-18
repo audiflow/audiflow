0eac08539f464004ee9989e59d4b6221407c0f41
HANAI tohru <tohru@reedom.com>
2024-02-18 19:09:32 +0900

    refactor

--- lib/providers/entities/podcast_episodes_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:seasoning/entities/entities.dart';
-import 'package:seasoning/providers/entities/podcast_channel_provider.dart';
-
-part 'podcast_episodes_provider.g.dart';
-
-@riverpod
-List<Episode> podcastEpisodes(PodcastEpisodesRef ref) =>
-    ref.watch(podcastChannelProvider)?.episodes ?? [];
