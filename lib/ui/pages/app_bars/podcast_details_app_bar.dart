880ec363121680b55cddb6b0cd77823fe4a9751d
HANAI tohru <tohru@reedom.com>
2024-03-16 21:43:15 +0900

    feat: add jumpToLastEpisode menu item

--- lib/ui/pages/app_bars/podcast_details_app_bar.dart
+++ lib/ui/pages/app_bars/podcast_details_app_bar.dart
@@ -5,9 +5,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:audiflow/core/l10n.dart';
 import 'package:audiflow/entities/podcast.dart';
 import 'package:audiflow/providers/podcast/podcast_info_provider.dart';
 import 'package:audiflow/services/podcast/podcast_service_provider.dart';
+import 'package:audiflow/ui/app/navigation_helper.dart';
 import 'package:audiflow/ui/pages/app_bars/podcast_page_header_image.dart';
 import 'package:audiflow/ui/widgets/placeholder_builder.dart';
 import 'package:flutter/material.dart';
@@ -71,9 +73,17 @@ class PodcastDetailsAppBar extends ConsumerWidget {
                       },
                     ),
             ),
-            IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {},
+            PopupMenuButton<String>(
+              onSelected: (_) {
+              },
+              itemBuilder: (BuildContext context) {
+                return ['settings'].map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(L10n.of(context)!.jumpToLastEpisode),
+                  );
+                }).toList();
+              },
             ),
           ],
           flexibleSpace: FlexibleSpaceBar(
