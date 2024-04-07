85f354812368732403da4f399d9209692976b905
HANAI tohru <tohru@reedom.com>
2024-04-07 13:29:27 +0900

    feat: add fashHash

---
+++ lib/core/hash.dart
@@ -0,0 +1,22 @@
+//  Copyright (c) 2024 by HANAI, Tohru.
+//  Copyright (c) 2024 Reedom, Inc.
+//  Additional contributions from project contributors.
+//  All rights reserved.
+//  Use of this source code is governed by a BSD-style license that can be
+//  found in the LICENSE file.
+
+int fastHash(String string) {
+  // ignore: avoid_js_rounded_ints
+  var hash = 0xcbf29ce484222325;
+
+  var i = 0;
+  while (i < string.length) {
+    final codeUnit = string.codeUnitAt(i++);
+    hash ^= codeUnit >> 8;
+    hash *= 0x100000001b3;
+    hash ^= codeUnit & 0xFF;
+    hash *= 0x100000001b3;
+  }
+
+  return hash;
+}
