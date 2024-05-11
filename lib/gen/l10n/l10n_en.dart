0a0c1bf5944544d5d290bdf96c48fdd55c36c1f2
HANAI tohru <tohru@reedom.com>
2024-05-11 19:06:16 +0900

    chore: gen loc

---
+++ lib/gen/l10n/l10n_en.dart
@@ -0,0 +1,317 @@
+// ignore_for_file: type=lint
+
+import 'package:intl/intl.dart' as intl;
+
+import 'l10n.dart';
+
+/// The translations for English (`en`).
+class L10nEn extends L10n {
+  L10nEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get locale => 'en';
+
+  @override
+  String get ok => 'OK';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get goBack => 'Go Back';
+
+  @override
+  String get continues => 'Continue';
+
+  @override
+  String get clearSearchButton => 'Clear search text';
+
+  @override
+  String get subscriptions => 'My Podcasts';
+
+  @override
+  String get popularPodcasts => 'Popular Podcasts';
+
+  @override
+  String get recentlyPlayed => 'Recently Played';
+
+  @override
+  String get latestEpisodes => 'Latest Episodes';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get chart => 'Chart';
+
+  @override
+  String get search => 'Search';
+
+  @override
+  String get library => 'Library';
+
+  @override
+  String get queue => 'Queue';
+
+  @override
+  String get nullSeason => 'Extras';
+
+  @override
+  String get clear => 'clear';
+
+  @override
+  String get season => 'Season';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get wifi => 'Wi-Fi';
+
+  @override
+  String get mobileData => 'Cellular Connection';
+
+  @override
+  String nEpisodes(int count) {
+    String _temp0 = intl.Intl.pluralLogic(
+      count,
+      locale: localeName,
+      other: '$count episodes',
+      one: '1 episodes',
+    );
+    return '$_temp0';
+  }
+
+  @override
+  String get sec => 's';
+
+  @override
+  String get min => 'm';
+
+  @override
+  String get hour => 'h';
+
+  @override
+  String nDaysAgo(int count) {
+    String _temp0 = intl.Intl.pluralLogic(
+      count,
+      locale: localeName,
+      other: '$count days ago',
+      one: '1 day ago',
+    );
+    return '$_temp0';
+  }
+
+  @override
+  String nHoursAgo(int count) {
+    String _temp0 = intl.Intl.pluralLogic(
+      count,
+      locale: localeName,
+      other: '$count hours ago',
+      one: '1 hour ago',
+    );
+    return '$_temp0';
+  }
+
+  @override
+  String nMinutesAgo(int count) {
+    String _temp0 = intl.Intl.pluralLogic(
+      count,
+      locale: localeName,
+      other: '$count minutes ago',
+      one: '1 minute ago',
+    );
+    return '$_temp0';
+  }
+
+  @override
+  String nSecondsAgo(int count) {
+    String _temp0 = intl.Intl.pluralLogic(
+      count,
+      locale: localeName,
+      other: '$count seconds ago',
+      one: '1 second ago',
+    );
+    return '$_temp0';
+  }
+
+  @override
+  String get justNow => 'Just now';
+
+  @override
+  String get viewModeEpisodes => 'Episodes';
+
+  @override
+  String get viewModeSeasons => 'Seasons';
+
+  @override
+  String get viewModePlayed => 'Played';
+
+  @override
+  String get viewModeUnplayed => 'Unplayed';
+
+  @override
+  String get viewModeDownloaded => 'Downloaded';
+
+  @override
+  String get viewSortOldestToNewest => 'Sort Oldest to Newest';
+
+  @override
+  String get jumpToLastEpisode => 'Jump to last episode';
+
+  @override
+  String get resume => 'Resume';
+
+  @override
+  String get play => 'Play';
+
+  @override
+  String get pause => 'Pause';
+
+  @override
+  String get playFromStart => 'Play from start';
+
+  @override
+  String get playLatest => 'Play latest';
+
+  @override
+  String get playAgain => 'Play again';
+
+  @override
+  String get tooltipPlay => 'Play episode';
+
+  @override
+  String get tooltipPause => 'Pause episode';
+
+  @override
+  String get settingsOnDemandDownloadOnPlayback => 'Download Audio on Playback';
+
+  @override
+  String get settingsOnDemandDownloadOnPlaybackDescription => 'If not pre-downloaded, audio will be fetched at the start of playback';
+
+  @override
+  String get settingsManualDownload => 'Manual Download';
+
+  @override
+  String get settingsManualDownloadDescription => 'When manually downloading episodes';
+
+  @override
+  String get settingsAutoDownload => 'Automatic Download';
+
+  @override
+  String get settingsAutoDownloadDescription => 'Automatically downloads in various situations to reduce wait times and hassle';
+
+  @override
+  String get settingsAutoDownloadEnabled => 'Enabled';
+
+  @override
+  String get settingsAutoDownloadRecent => 'Number of Recently Published Episodes to Fetch';
+
+  @override
+  String get settingsAutoDownloadSubject => '';
+
+  @override
+  String get settingsAutoDownloadQueuedEpisodes => 'Episodes added to queue manually';
+
+  @override
+  String get settingsAutoDownloadIncludesAdhoc => 'Includes episodes automatically added to queue';
+
+  @override
+  String get settingsAutoDelete => 'Automatic Deletion';
+
+  @override
+  String get settingsAutoDeleteDescription => 'Automatically delete episodes after playback';
+
+  @override
+  String get settingsAutoDeleteAfter => 'After Playback';
+
+  @override
+  String get settingsAutoDeleteAfterDescription => 'Automatically delete episodes after playback';
+
+  @override
+  String get settingsAutoDeleteAfterNever => 'Never';
+
+  @override
+  String get settingsAutoDeleteAfter1day => '1 day';
+
+  @override
+  String get settingsAutoDeleteAfter3days => '3 days';
+
+  @override
+  String get settingsAutoDeleteAfter7days => '7 days';
+
+  @override
+  String get settingsAutoDeleteAfter14days => '14 days';
+
+  @override
+  String get settingsAutoDeleteAfter30days => '30 days';
+
+  @override
+  String get settingsWarnMobileData => 'Warn on Cellular Connection';
+
+  @override
+  String get settingsWarnMobileDataDescription => 'For those concerned about data usage, such as with metered plans';
+
+  @override
+  String get settingsWarnWifi => 'Warn when not connected to Wi-Fi';
+
+  @override
+  String get settingsWifiOnly => 'Wi-Fi Connection Only';
+
+  @override
+  String get settingsOpml => 'OPML Import/Export';
+
+  @override
+  String get settingsOpmlDescription => 'Import or export podcasts via OPML file, a de-facto standard file for podcast subscriptions';
+
+  @override
+  String get settingsOpmlImport => 'Import Podcasts via OPML File';
+
+  @override
+  String get settingsOpmlExport => 'Export Podcasts to OPML File';
+
+  @override
+  String get titleNoFiFi => 'Not Connected to Wi-Fi';
+
+  @override
+  String get titleCellularConnection => 'Cellular Connection';
+
+  @override
+  String get captionStreamingNoWifi => 'Since Wi-Fi is not connected, audio data will be fetched over cellular data.';
+
+  @override
+  String get captionDownloadNoWifi => '\'Since Wi-Fi is not connected, the episode will be downloaded over cellular data.\'';
+
+  @override
+  String get captionWarnSettingNavigation => 'You can change this warning in the settings';
+
+  @override
+  String get proceedPlaying => 'Play anyway';
+
+  @override
+  String get proceedDownload => 'Proceed';
+
+  @override
+  String get downloadAllEpisodes => 'Download All Episodes';
+
+  @override
+  String get downloadUnplayedEpisodes => 'Download Unplayed Episodes';
+
+  @override
+  String get semantics_podcast_details_header => 'Podcast details and episodes page';
+
+  @override
+  String get search_for_podcasts_hint => 'Search for podcasts';
+
+  @override
+  String get semantic_announce_searching => 'Searching, please wait.';
+
+  @override
+  String get no_search_results_message => 'No podcasts found';
+
+  @override
+  String get podcast_funding_dialog_header => 'Podcast Funding';
+
+  @override
+  String get podcast_funding_consent_message => 'This funding link will take you to an external site where you will be able to directly support the show. Links are provided by the podcast authors and is not controlled by Anytime.';
+}
---
+++ lib/gen/l10n/l10n_ja.dart
@@ -0,0 +1,287 @@
+// ignore_for_file: type=lint
+
+import 'package:intl/intl.dart' as intl;
+
+import 'l10n.dart';
+
+/// The translations for Japanese (`ja`).
+class L10nJa extends L10n {
+  L10nJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get locale => 'ja';
+
+  @override
+  String get ok => 'OK';
+
+  @override
+  String get cancel => 'キャンセル';
+
+  @override
+  String get goBack => '戻る';
+
+  @override
+  String get continues => '続行';
+
+  @override
+  String get clearSearchButton => 'クリア';
+
+  @override
+  String get subscriptions => 'フォロー中';
+
+  @override
+  String get popularPodcasts => '人気ポッドキャスト';
+
+  @override
+  String get recentlyPlayed => '再生履歴';
+
+  @override
+  String get latestEpisodes => '最新エピソード';
+
+  @override
+  String get home => 'ホーム';
+
+  @override
+  String get chart => 'チャート';
+
+  @override
+  String get search => '検索';
+
+  @override
+  String get library => 'Library';
+
+  @override
+  String get queue => '連続再生キュー';
+
+  @override
+  String get nullSeason => 'その他';
+
+  @override
+  String get clear => 'クリア';
+
+  @override
+  String get season => 'シーズン';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get wifi => 'Wi-Fi';
+
+  @override
+  String get mobileData => 'モバイル回線';
+
+  @override
+  String nEpisodes(int count) {
+    return '$countエピソード';
+  }
+
+  @override
+  String get sec => '秒';
+
+  @override
+  String get min => '分';
+
+  @override
+  String get hour => '時間';
+
+  @override
+  String nDaysAgo(int count) {
+    return '$count日前';
+  }
+
+  @override
+  String nHoursAgo(int count) {
+    return '$count時間前';
+  }
+
+  @override
+  String nMinutesAgo(int count) {
+    return '$count分前';
+  }
+
+  @override
+  String nSecondsAgo(int count) {
+    return '$count秒前';
+  }
+
+  @override
+  String get justNow => 'たった今';
+
+  @override
+  String get viewModeEpisodes => 'エピソード';
+
+  @override
+  String get viewModeSeasons => 'シーズン';
+
+  @override
+  String get viewModePlayed => '再生済み';
+
+  @override
+  String get viewModeUnplayed => '未再生';
+
+  @override
+  String get viewModeDownloaded => 'ダウンロード済み';
+
+  @override
+  String get viewSortOldestToNewest => '古い順に並び替え';
+
+  @override
+  String get jumpToLastEpisode => '最後に聴いたエピソードへ';
+
+  @override
+  String get resume => '再開';
+
+  @override
+  String get play => '再生';
+
+  @override
+  String get pause => '一時停止';
+
+  @override
+  String get playFromStart => '最初から再生';
+
+  @override
+  String get playLatest => '最新のエピソードを再生';
+
+  @override
+  String get playAgain => '再生';
+
+  @override
+  String get tooltipPlay => 'エピソードを再生';
+
+  @override
+  String get tooltipPause => 'エピソード再生を一時停止';
+
+  @override
+  String get settingsOnDemandDownloadOnPlayback => '再生時の音源取得';
+
+  @override
+  String get settingsOnDemandDownloadOnPlaybackDescription => '事前ダウンロードがない場合、再生開始時に音源を取得します';
+
+  @override
+  String get settingsManualDownload => '手動ダウンロード';
+
+  @override
+  String get settingsManualDownloadDescription => 'エピソードを手動でダウンロードするとき';
+
+  @override
+  String get settingsAutoDownload => '自動ダウンロード';
+
+  @override
+  String get settingsAutoDownloadDescription => '様々な場面で自動的にダウンロードすることで、待ち時間や煩わしさを軽減します';
+
+  @override
+  String get settingsAutoDownloadEnabled => '有効';
+
+  @override
+  String get settingsAutoDownloadRecent => '直近公開エピソード取得件数';
+
+  @override
+  String get settingsAutoDownloadSubject => '';
+
+  @override
+  String get settingsAutoDownloadQueuedEpisodes => 'キューに手動で追加したエピソード';
+
+  @override
+  String get settingsAutoDownloadIncludesAdhoc => 'キューに自動追加されたエピソードも対象';
+
+  @override
+  String get settingsAutoDelete => '自動削除';
+
+  @override
+  String get settingsAutoDeleteDescription => '再生後にエピソードを自動で削除';
+
+  @override
+  String get settingsAutoDeleteAfter => '再生後';
+
+  @override
+  String get settingsAutoDeleteAfterDescription => '再生後にエピソードを自動で削除';
+
+  @override
+  String get settingsAutoDeleteAfterNever => 'しない';
+
+  @override
+  String get settingsAutoDeleteAfter1day => '1日';
+
+  @override
+  String get settingsAutoDeleteAfter3days => '3日';
+
+  @override
+  String get settingsAutoDeleteAfter7days => '7日';
+
+  @override
+  String get settingsAutoDeleteAfter14days => '14日';
+
+  @override
+  String get settingsAutoDeleteAfter30days => '30日';
+
+  @override
+  String get settingsWarnMobileData => 'モバイル回線接続時は警告を表示';
+
+  @override
+  String get settingsWarnMobileDataDescription => '従量課金制など通信量が気になる方に';
+
+  @override
+  String get settingsWarnWifi => 'Wi-Fi未接続時に警告を表示';
+
+  @override
+  String get settingsWifiOnly => 'Wi-Fi接続時のみ';
+
+  @override
+  String get settingsOpml => 'OPML Import/Export';
+
+  @override
+  String get settingsOpmlDescription => 'OPMLファイルを使ってポッドキャストをインポート/エクスポート';
+
+  @override
+  String get settingsOpmlImport => 'Import Podcasts via OPML File';
+
+  @override
+  String get settingsOpmlExport => 'Export Podcasts to OPML File';
+
+  @override
+  String get titleNoFiFi => 'Wi-Fi未接続';
+
+  @override
+  String get titleCellularConnection => 'モバイル回線接続中';
+
+  @override
+  String get captionStreamingNoWifi => 'WiFiが接続されていないため、モバイル回線で音声データを取得します。';
+
+  @override
+  String get captionDownloadNoWifi => '\'WiFiが接続されていないため、モバイル回線でエピソードをダウンロードします。\'';
+
+  @override
+  String get captionWarnSettingNavigation => 'この警告表示有無は設定で変更できます';
+
+  @override
+  String get proceedPlaying => '再生する';
+
+  @override
+  String get proceedDownload => '続行';
+
+  @override
+  String get downloadAllEpisodes => 'すべてのエピソードをダウンロード';
+
+  @override
+  String get downloadUnplayedEpisodes => '未再生のエピソードをダウンロード';
+
+  @override
+  String get semantics_podcast_details_header => 'ポッドキャスト詳細';
+
+  @override
+  String get search_for_podcasts_hint => 'ポッドキャストを検索…';
+
+  @override
+  String get semantic_announce_searching => '検索しています…';
+
+  @override
+  String get no_search_results_message => '該当するポッドキャストが見つかりませんでした';
+
+  @override
+  String get podcast_funding_dialog_header => 'ポッドキャストへのサポート';
+
+  @override
+  String get podcast_funding_consent_message => 'このポッドキャストをサポートするために、以下のリンクからサポートを行うことができます。';
+}
