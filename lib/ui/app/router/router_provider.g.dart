2d4e3053b88b358d18d033794e8481b4cb497f01
HANAI tohru <tohru@reedom.com>
2024-05-11 19:07:24 +0900

    update: routing

--- lib/ui/app/router/router_provider.g.dart
+++ lib/ui/app/router/router_provider.g.dart
@@ -2,7 +2,7 @@
 
 // ignore_for_file: type=lint, duplicate_ignore
 
-part of 'router.dart';
+part of 'router_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
