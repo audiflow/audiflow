ae186364dc5e0efaf7fb626b04cdbc8e1e4388ee
HANAI tohru <tohru@reedom.com>
2024-07-18 16:13:51 +0900

    feat: add apiCacheDirProvider

---
+++ lib/core/api_cache_dir.dart
@@ -0,0 +1,8 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'api_cache_dir.g.dart';
+
+@Riverpod(keepAlive: true)
+String apiCacheDir(ApiCacheDirRef ref) {
+  throw UnimplementedError();
+}
---
+++ lib/core/api_cache_dir.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_cache_dir.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$apiCacheDirHash() => r'7913a60894217fa0acc6bf065840427705bc61ed';
+
+/// See also [apiCacheDir].
+@ProviderFor(apiCacheDir)
+final apiCacheDirProvider = Provider<String>.internal(
+  apiCacheDir,
+  name: r'apiCacheDirProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$apiCacheDirHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ApiCacheDirRef = ProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
