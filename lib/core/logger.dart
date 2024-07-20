2ecb56a20a5d0d8f02d2812c9c31ab487c0e2e96
HANAI tohru <tohru@reedom.com>
2024-07-20 12:33:24 +0900

    update: logger option

--- lib/core/logger.dart
+++ lib/core/logger.dart
@@ -3,5 +3,7 @@ import 'package:logger/logger.dart';
 final logger = Logger(
   printer: PrettyPrinter(
     printEmojis: false,
+    colors: false,
+    dateTimeFormat: DateTimeFormat.onlyTimeAndSinceStart,
   ),
 );
