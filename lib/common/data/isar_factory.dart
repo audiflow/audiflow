0eaeca1efcd1302041a77647348af7c378bcc999
HANAI tohru <tohru@reedom.com>
2024-07-23 23:52:52 +0900

    refactor: change to IsarFactory

---
+++ lib/common/data/isar_factory.dart
@@ -0,0 +1,36 @@
+import 'package:audiflow/entities/playing_episode.dart';
+import 'package:audiflow/features/download/model/downloadable.dart';
+import 'package:audiflow/features/feed/model/model.dart';
+import 'package:audiflow/features/queue/model/queue.dart';
+import 'package:isar/isar.dart';
+
+class IsarFactory {
+  IsarFactory._();
+
+  static Future<Isar> create(String storageDir) async {
+    return Isar.getInstance() ??
+        await Isar.open(
+          [
+            BlockSchema,
+            DownloadableSchema,
+            EpisodeSchema,
+            EpisodeStatsSchema,
+            FeedUrlSchema,
+            FundingSchema,
+            LockedSchema,
+            PersonSchema,
+            PlayingEpisodeSchema,
+            PodcastSchema,
+            PodcastStatsSchema,
+            PodcastViewStatsSchema,
+            QueueSchema,
+            TranscriptUrlSchema,
+            TranscriptSchema,
+            SubtitleSchema,
+            ValueSchema,
+            ValueRecipientSchema,
+          ],
+          directory: storageDir,
+        );
+  }
+}
