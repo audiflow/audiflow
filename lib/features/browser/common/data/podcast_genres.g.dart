9e40525f41e66b3a8066be2911b7b27a1c25e2ef
HANAI tohru <tohru@reedom.com>
2024-07-25 00:01:47 +0900

    feat: genres

---
+++ lib/features/browser/common/data/podcast_genres.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'podcast_genres.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$podcastGenresHash() => r'3cf8e5fda1523109ec6046775bfda40bc2dd48a8';
+
+/// See also [PodcastGenres].
+@ProviderFor(PodcastGenres)
+final podcastGenresProvider =
+    NotifierProvider<PodcastGenres, PodcastGenresState>.internal(
+  PodcastGenres.new,
+  name: r'podcastGenresProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$podcastGenresHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PodcastGenres = Notifier<PodcastGenresState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
