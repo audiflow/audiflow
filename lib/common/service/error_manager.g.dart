b9179240c2e05593f536f931b8c43fc0f5bf1fd1
HANAI tohru <tohru@reedom.com>
2024-08-06 16:41:05 +0900

    refactor: app exceptions

---
+++ lib/common/service/error_manager.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'error_manager.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$errorManagerHash() => r'51f340b2462239778d03e51337804e1eb4275400';
+
+/// See also [ErrorManager].
+@ProviderFor(ErrorManager)
+final errorManagerProvider =
+    StreamNotifierProvider<ErrorManager, AppException>.internal(
+  ErrorManager.new,
+  name: r'errorManagerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$errorManagerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ErrorManager = StreamNotifier<AppException>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
