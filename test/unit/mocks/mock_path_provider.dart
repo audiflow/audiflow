d8a358e90a9331a4703e4290051be9ad8bf6c5dc
HANAI tohru <tohru@reedom.com>
2024-08-10 10:40:10 +0900

    refactor: remove deprecated comment

--- test/unit/mocks/mock_path_provider.dart
+++ test/unit/mocks/mock_path_provider.dart
@@ -1,7 +1,3 @@
-
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'dart:io';
 
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
