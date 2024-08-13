7b7e9484ca1ecebdbee95ee8362111e5763bcb46
HANAI tohru <tohru@reedom.com>
2024-08-13 21:16:28 +0900

    feat: add queueTopEpisodeProvider

---
+++ lib/features/queue/data/queue_top_episode_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'queue_top_episode_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$queueTopEpisodeHash() => r'e7f16ff6665be01c232442f69cef1e9a43c3e880';
+
+/// See also [QueueTopEpisode].
+@ProviderFor(QueueTopEpisode)
+final queueTopEpisodeProvider =
+    AutoDisposeNotifierProvider<QueueTopEpisode, Episode?>.internal(
+  QueueTopEpisode.new,
+  name: r'queueTopEpisodeProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$queueTopEpisodeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$QueueTopEpisode = AutoDisposeNotifier<Episode?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
