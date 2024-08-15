b9bf8e906642aedfbf8667d0aa328e8cd3d352f2
HANAI tohru <tohru@reedom.com>
2024-08-16 01:36:08 +0900

    fix: miniPlayerHeightProvider

--- lib/features/player/ui/expandable_player/mini_player_height_provider.dart
+++ lib/features/player/ui/expandable_player/mini_player_height_provider.dart
@@ -1,19 +1,13 @@
-import 'package:flutter/foundation.dart';
+import 'package:audiflow/features/player/ui/expandable_player/expandable_player_controller.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'mini_player_height_provider.g.dart';
 
 const double playerMinHeight = 80;
 const miniPlayerPercentageDeclaration = 0.2;
-final miniPlayerExpandProgress = ValueNotifier<double>(0);
 
-@Riverpod(keepAlive: true)
+@riverpod
 double miniPlayerHeight(MiniPlayerHeightRef ref) {
-  void onValueChange() {
-    ref.state = playerMinHeight * miniPlayerExpandProgress.value;
-  }
-
-  miniPlayerExpandProgress.addListener(onValueChange);
-  ref.onDispose(() => miniPlayerExpandProgress.removeListener(onValueChange));
-  return miniPlayerExpandProgress.value;
+  final expandablePlayerState = ref.watch(expandablePlayerControllerProvider);
+  return expandablePlayerState.episode == null ? 0 : playerMinHeight;
 }
