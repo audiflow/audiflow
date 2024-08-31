76be4f7d95370aebfe39f7415ed6582cf2905889
HANAI tohru <tohru@reedom.com>
2024-08-31 10:54:45 +0900

    feat: add LastEpisodeService

---
+++ lib/features/player/service/last_episode_service.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'last_episode_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$lastEpisodeServiceHash() =>
+    r'2df59b560ac9f053bd8b86d09bfa2f9022399881';
+
+/// See also [lastEpisodeService].
+@ProviderFor(lastEpisodeService)
+final lastEpisodeServiceProvider =
+    AutoDisposeProvider<LastEpisodeService>.internal(
+  lastEpisodeService,
+  name: r'lastEpisodeServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$lastEpisodeServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef LastEpisodeServiceRef = AutoDisposeProviderRef<LastEpisodeService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
