da0600a2180dd15eebbfe691ad5e86b4af397846
HANAI tohru <tohru@reedom.com>
2024-10-27 14:21:49 +0900

    fix: main files

--- lib/main_stg.dart
+++ lib/main_stg.dart
@@ -1,9 +1,7 @@
 import 'firebase_options_stg.dart';
-import 'constants/flavors.dart';
 
 import 'main.dart' as runner;
 
 Future<void> main() async {
-  F.appFlavor = Flavor.stg;
   await runner.runMainApp(firebaseOptions: DefaultFirebaseOptions.currentPlatform);
 }
