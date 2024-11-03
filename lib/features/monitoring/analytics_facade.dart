2996d9f55a8acd49ee6e9978b5fc5e5a22640373
HANAI tohru <tohru@reedom.com>
2024-11-03 09:26:04 +0900

    feat: add monitoring classes

---
+++ lib/features/monitoring/analytics_facade.dart
@@ -0,0 +1,78 @@
+import 'package:audiflow/features/monitoring/analytics_client.dart';
+import 'package:audiflow/features/monitoring/logger_analytics_client.dart';
+import 'package:audiflow/features/monitoring/mixpanel_analytics_client.dart';
+import 'package:flutter/foundation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'analytics_facade.g.dart';
+
+@Riverpod(keepAlive: true)
+AnalyticsFacade analyticsFacade(AnalyticsFacadeRef ref) {
+  final mixpanelAnalyticsClient = ref.watch(mixpanelAnalyticsClientProvider);
+  return AnalyticsFacade([
+    mixpanelAnalyticsClient,
+    if (!kReleaseMode) LoggerAnalyticsClient(),
+  ]);
+}
+
+class AnalyticsFacade implements AnalyticsClient {
+  const AnalyticsFacade(this.clients);
+
+  final List<AnalyticsClient> clients;
+
+  Future<void> _dispatch(
+      Future<void> Function(AnalyticsClient client) work) async {
+    for (var client in clients) {
+      await work(client);
+    }
+  }
+
+  @override
+  Future<void> setAnalyticsCollectionEnabled({required bool enabled}) =>
+      _dispatch(
+        (c) => c.setAnalyticsCollectionEnabled(enabled: enabled),
+      );
+
+  @override
+  Future<void> identifyUser(String userId) => _dispatch(
+        (c) => c.identifyUser(userId),
+      );
+
+  @override
+  Future<void> resetUser() => _dispatch(
+        (c) => c.resetUser(),
+      );
+
+  @override
+  Future<void> trackScreenView(String routeName, String action) => _dispatch(
+        (c) => c.trackScreenView(routeName, action),
+      );
+
+  @override
+  Future<void> trackHomeView() => _dispatch(
+        (c) => c.trackHomeView(),
+      );
+
+  @override
+  Future<void> trackOpenApp() => _dispatch(
+        (c) => c.trackOpenApp(),
+      );
+
+  @override
+  Future<void> trackStartPlayback({
+    required int pid,
+    required int eid,
+    required int? sid,
+    required String title,
+    required DateTime pubDate,
+  }) =>
+      _dispatch(
+        (c) => c.trackStartPlayback(
+          pid: pid,
+          eid: eid,
+          sid: sid,
+          title: title,
+          pubDate: pubDate,
+        ),
+      );
+}
---
+++ lib/features/monitoring/logger_analytics_client.dart
@@ -0,0 +1,67 @@
+import 'package:audiflow/features/monitoring/analytics_client.dart';
+import 'package:audiflow/utils/logger.dart';
+
+class LoggerAnalyticsClient implements AnalyticsClient {
+  LoggerAnalyticsClient();
+
+  String? _userId;
+  bool _enabled = false;
+
+  @override
+  Future<void> identifyUser(String userId) async {
+    _userId = userId;
+    if (_enabled) {
+      logger.i('[analytics] Identifying user with id: $userId');
+    }
+  }
+
+  @override
+  Future<void> resetUser() async {
+    _userId = '';
+    if (_enabled) {
+      logger.i('[analytics] Resetting user');
+    }
+  }
+
+  @override
+  Future<void> setAnalyticsCollectionEnabled({required bool enabled}) async {
+    _enabled = enabled;
+    logger.i('[analytics] Setting analytics collection enabled to: $enabled');
+  }
+
+  @override
+  Future<void> trackScreenView(String routeName, String action) async {
+    if (_enabled) {
+      logger.i('[analytics] trackScreenView view: $routeName, action: $action');
+    }
+  }
+
+  @override
+  Future<void> trackHomeView() async {
+    if (_enabled) {
+      logger.i('[analytics] trackHomeView');
+    }
+  }
+
+  @override
+  Future<void> trackOpenApp() async {
+    if (_enabled) {
+      logger.i('[analytics] trackOpenApp');
+    }
+  }
+
+  @override
+  Future<void> trackStartPlayback(
+      {required int pid,
+      required int eid,
+      required int? sid,
+      required String title,
+      required DateTime pubDate}) async {
+    if (_enabled) {
+      logger.i('[analytics] trackStartPlayback('
+          'pid: $pid, eid: $eid, sid: $sid, '
+          'title: $title, pubDate: $pubDate'
+          ')');
+    }
+  }
+}
