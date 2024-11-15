e996bb5784073667fc2fe3678b17d075e1b55a49
HANAI tohru <tohru@reedom.com>
2024-11-16 01:04:26 +0900

    refactor: remove unused files

--- lib/services/audio/audio_player_service_provider.dart
+++
@@ -1,6 +0,0 @@
-// part 'audio_player_service_provider.g.dart';
-//
-// final audioPlayerServiceProvider =
-//     NotifierProvider<AudioPlayerService, AudioPlayerState?>(
-//   MobileAudioPlayerService.new,
-// );
