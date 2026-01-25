8a6440d7a86a2f55aece908d884237cb3f9be2d4
HANAI, Tohru <tohru@reedom.com>
2026-01-25 13:17:21 +0900

    feat(cli): scaffold audiflow_cli package

---
+++ packages/audiflow_cli/bin/audiflow_cli.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  print('audiflow_cli - coming soon');
+  exit(0);
+}
---
+++ packages/audiflow_cli/lib/audiflow_cli.dart
@@ -0,0 +1,5 @@
+/// CLI tools for debugging Audiflow features.
+library;
+
+export 'src/models/extraction_result.dart';
+export 'src/adapters/episode_adapter.dart';
