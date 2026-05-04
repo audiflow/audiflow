e8c7bb0ba3fd896c3bcdeabc8d73726501a72692
HANAI, Tohru <tohru@reedom.com>
2026-05-04 21:41:59 +0900

    chore(force-update): scaffold feature module + env vars

---
+++ packages/audiflow_app/lib/features/force_update/constants.dart
@@ -0,0 +1,6 @@
+const forceUpdateRefreshInterval = Duration(hours: 6);
+const forceUpdateFetchTimeout = Duration(seconds: 5);
+const forceUpdateCacheKey = 'force_update_config_v1';
+const forceUpdateLastFetchKey = 'force_update_last_fetch_at';
+const forceUpdateConfigUrlEnv = 'FORCE_UPDATE_CONFIG_URL';
+const forceUpdateSupportedSchemaVersion = 1;
