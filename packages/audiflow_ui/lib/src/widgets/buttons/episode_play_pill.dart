4372dc7aed51ff38b2d158d01d3f1ac96460b58d
HANAI, Tohru <tohru@reedom.com>
2026-05-09 01:45:01 +0900

    fix(ui): bump EpisodePlayPill padding to 6h/4v

--- packages/audiflow_ui/lib/src/widgets/buttons/episode_play_pill.dart
+++ packages/audiflow_ui/lib/src/widgets/buttons/episode_play_pill.dart
@@ -50,7 +50,7 @@ class EpisodePlayPill extends StatelessWidget {
         customBorder: const StadiumBorder(),
         onTap: onPressed,
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 2),
+          padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
           child: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
